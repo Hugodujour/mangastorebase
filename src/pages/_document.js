233bef7aa3aebad861a550d8babe66d3b78a14f1
Hugo <hugogreg@live.fr>
2023-03-15 17:09:31 +0100

    ajout snipcart

---
+++ src/pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+    <link rel="preconnect" href="<https://app.snipcart.com>"/>
+    <link rel="preconnect" href="<https://cdn.snipcart.com>"/>
+    <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.css" />
+    
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <script src="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.js"></script>
+        <div hidden id="snipcart" data-api-key="YjkwMDI4MzUtNDE3ZS00MTRkLThhYjYtNmNiNTM4ODIzNmNlNjM4MTQzMDkwMzE2MTUyNzQ5"></div>
+      </body>
+    </Html>
+  )
+}
