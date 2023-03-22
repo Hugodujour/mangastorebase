f7d54ba94c0b378e53fa4c8706fd1ba3b5d93c15
Hugo <hugogreg@live.fr>
2023-03-22 20:31:02 +0100

    with auth

---
+++ src/auth/withAuth.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import router from 'next/router';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import initFirebase from '../config';
+
+initFirebase();
+const auth = firebase.auth();
+
+const withAuth = Component => {
+    const displayName = Component.displayName || Component.name || 'Component';
+    const WrappedComponent = props => {
+      useEffect(() => {
+        auth.onAuthStateChanged(authUser => {
+          if (!authUser) {
+            router.push('/signin');
+          }
+        });
+      }, []);
+  
+      return (
+        <div>
+          <Component {...props} />
+        </div>
+      );
+    };
+  
+    WrappedComponent.displayName = `withAuth(${displayName})`;
+    return WrappedComponent;
+  };
+
+
+export default withAuth;
