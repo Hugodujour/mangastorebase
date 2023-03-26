02095d7cd0950e4f7cde452b237638f61f80b7ad
Hugo <hugogreg@live.fr>
2023-03-26 15:32:49 +0200

    ajout rgpd

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,17 @@
+import Link from 'next/link';
 import styles from './Footer.module.scss';
 
 const Footer = ({ ...rest }) => {
   return (
     <footer className={styles.footer} {...rest}>
       <p>
-        &copy; Manga Store, {new Date().getFullYear()} 
+        &copy; Manga Store, {new Date().getFullYear()} -
       </p>
+      <Link href="/about/about">
+        <a>Politique de confidentialité</a>
+      </Link>
     </footer>
+    
   )
 }
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import '@styles/globals.scss'
 import Script from 'next/script';
+import CookieConsent from "react-cookie-consent";
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script src="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.js" />
       <Component {...pageProps} />
+      <CookieConsent>Ce site utilise des cookies pour améliorer l&apos;expérience utilisateur.</CookieConsent>
+
     </>
   );
 }
